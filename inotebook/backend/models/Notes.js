af9317ed02e7377ff8b24761935c0e469f41e242
ritik <54032937+ritiktyagi1@users.noreply.github.com>
2022-01-20 02:51:48 +0530

    storing data into database

---
+++ inotebook/backend/models/Notes.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const NotesSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    tag: {
+        type: String,
+        default: "General"
+    },
+    date: {
+        type: Date,
+        default: date.now
+    }
+  });
+
+  module.exports= mongoose.model('user', NotesSchema);
