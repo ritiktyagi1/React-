679f65bc073b25adafee8ae0ee59bccc67ee14f2
ritik <54032937+ritiktyagi1@users.noreply.github.com>
2022-01-23 02:59:38 +0530

    Fetching notes functio

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -1,5 +1,6 @@
 const connectedToMongo = require('./db');
 const express = require('express')
+var cors = require('cors')
 
 connectedToMongo();
 
@@ -7,6 +8,7 @@ const app = express()
 const port = 5000
 
 
+app.use(cors())
 app.use(express.json())
 
 // Available Routes
